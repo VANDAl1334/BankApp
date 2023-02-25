6789247e277cc51e4e0dd165512a2d7adf38fcad
Данил Ш <vandal2281335@gmail.com>
2023-02-26 02:53:51 +0300

    потраченава времене жоль

---
+++ Desktop/GitHub/BankApp_3/BankApp/Libs/DB.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Libs
+{
+    static class DB
+    {
+        static MySqlConnection Connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;database=bankdb");
+        public static DataTable Table = new DataTable();
+        public static MySqlDataAdapter Adapter = new MySqlDataAdapter();
+        public static MySqlCommand Command = new MySqlCommand();
+
+        static public void OpenConnection()
+        {
+            if(Connection.State == System.Data.ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+        }
+
+        static public void CloseConnection()
+        {
+            if (Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+        }
+
+        static public MySqlConnection GetConnection()
+        {
+            return Connection;
+        }
+    }
+}
---
+++ Desktop/GitHub/BankApp_3/BankApp/Libs/LibClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Libs
+{
+    class LibClient : LibUser
+    {
+        public List<LibBill> bills;
+        public LibClient(string login) : base(login)
+        {
+        }
+    }
+}
